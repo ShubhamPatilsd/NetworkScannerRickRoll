2422aab254f8956479fb9e987731aa43eb2f5536
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2020-10-11 18:48:54 -0700

    Added some stuff to make sure if anyone looking at it understands what to change

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,19 @@ public class Main {
         while (true) {
             try{
                 ipaddresses.clear();
+		
+		//Change this to whatever the starting of your LAN Network is. For example, if the target IP is "192.168.0.23", you would just put the "192.168.0" part because the program tries and scans the network and zooms through and increases the last part of the "192.168.0" part by one. FOr example, "192.168.0" will be addded with "192.168.0.1". This goes on until 254 of the last park
                 s = checkHosts("192.168.0");
             }catch(ConnectException c){
                 c.printStackTrace();
             }
             System.out.println(s);
+
+		//This is what the target IP is. It is what is trying to be checked if it is online on the network.
             if (s.contains("192.168.0.26") && !hasBeenDetectedBefore) {
                 hasBeenDetectedBefore=true;
+
+		//Change the link to whatever you want safari to open		
                 rt.exec("open https://www.youtube.com/watch?v=oHg5SJYRHA0 -a Safari");
             }else{
                 hasBeenDetectedBefore=false;
